76393c0c89e6fa9f52cef64aef9e89ba1e8ba997
Skylar <lorayne911@gmail.com>
2022-05-02 00:26:47 +0300

    Create a function that calculates an employee's yearly salary given number of hours per week and money made per hour.

---
+++ src/main/java/org/classroom/SalaryCalculator.java
@@ -0,0 +1,21 @@
+package org.classroom;
+
+public class SalaryCalculator {
+    //    Create a function that calculates an employee's yearly salary given number of hours per week and money made per hour.
+//     Add an input that accounts for vacation days. Employee is unpaid on vacation days. 1 Vacation day = 8 hours of work
+    public static int calculateTotalSalary(int hourlyRate, int hoursWorked, int vacationDays){
+        int hours = hoursWorked;
+        int vacationDeductions = vacationDays * hourlyRate;
+        int rates = hourlyRate;
+        int salary = hours * rates - vacationDeductions ;
+        return salary;
+
+    }
+
+    public static void main(String[] args) {
+        int  monthlySalary = calculateTotalSalary(25, 48, 0);
+        System.out.println(monthlySalary);
+        int yearlySalary = monthlySalary *12 ;
+        System.out.println(yearlySalary);
+    }
+}
